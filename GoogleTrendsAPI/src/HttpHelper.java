94906e865fafc005630fa616231a15684be4a593
Andrew Tong <andrewtong19@gmail.com>
2016-02-09 15:42:58 -0800

    Create HttpHelper.java

---
+++ GoogleTrendsAPI/src/HttpHelper.java
@@ -0,0 +1,25 @@
+package at.GT.HttpHelper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+
+public class HttpHelper {
+	
+	public static String ReadEntity(HttpEntity entity) throws UnsupportedOperationException, IOException{
+	  String string;
+		StringBuilder output = new StringBuilder();
+		if (entity.getContent() != null){
+			BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
+			
+	        while ((string = reader.readLine()) != null) {
+	            output.append(string);
+	        }
+	        reader.close();
+		}
+		return output.toString();
+	}
+
+}
